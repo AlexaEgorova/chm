40366235674241d439c37e52a8b0856bd0a0a5e1
AlexaEgorova <37927173+AlexaEgorova@users.noreply.github.com>
2018-10-30 21:37:55 +0300

    Update kramer_check.py

--- kramer_check.py
+++ kramer_check.py
@@ -6,7 +6,7 @@ def det(a1):
     res = 1
     n = len(a)
     for i in range(n):
-        j = max(list(range(i, n)))  # key=lambda k: abs(a[k][i]))
+        j = max(list(range(i, n)))
         if i != j:
             a[i], a[j] = a[j].copy(), a[i].copy()
             res *= -1
@@ -24,20 +24,16 @@ def kramer(a, v):
     x = np.ones(n)
     deltas = np.ones(n)
     m = np.copy(a)
-    m_t = np.transpose(m)
+    a_t = a.transpose()
     delta = det(m)
-
     for i in range(n):
+        m_t = a_t.copy()
         m_t[i] = np.copy(v)
-        deltas[i] = det(m_t)
-        x[i] = deltas[i] / delta
+        #deltas[i] = det(m_t)
+        deltas[i] = np.linalg.det(m_t)
+        x[i] = deltas[i] / delta + 0
         print(f"x[{i}] = {x[i] + 0}")
-
     return x
-
-
-def relax():
-    pass
 
 
 a = np.array([[2., 5.], [1., -10.]])
