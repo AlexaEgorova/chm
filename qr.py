0056b2fdc96fec953ac1247ea586dcd5d262e37d
AlexaEgorova <37927173+AlexaEgorova@users.noreply.github.com>
2018-11-15 08:26:58 +0300

    Update qr.py

--- qr.py
+++ qr.py
@@ -14,12 +14,13 @@ def qr(A):
         U[i:, i:] = ufind(A[i:, i])
         Q = np.dot(Q, U)
         A = np.dot(U, A)
-    #print("Q:\n", Q)
+    print("Q:\n", Q)
     for i in range(0, n):
         for j in range(0, n):
             A[i][j] = round(A[i][j], 5)
-    #print("A:\n", A)
-    return Q, A
+    print("A:\n", A)
+    return A
+
 
 def ufind(cln):
     v = cln / (cln[0] + np.copysign(norm(cln), cln[0]))
@@ -29,48 +30,26 @@ def ufind(cln):
     return U
 
 def backsub(A, b):
-    n = len(A)
-    for i in range(0, n):
-        if A[i][i] == 0:
-            print("Вырожденная матрица!")
-        else:
-            x = np.zeros(n)
-            for k in range(n):
-                if A[k, k] != 1:
-                    A[k, :] *= 1 / A[k, k]
-                    b[k] *= A[k, k]
-                for row in range(k + 1, n):
-                    A[row, :] -= A[k, :] * A[row, k]
-                    b[row] -= b[k] * A[row, k]
-                for j in range(n-1, 0, -1):
-                    for i in range(j-1, -1, -1):
-                        if A[i][j]:
-                            A[i, :] -= A[j, :] * A[i][j]
-                            b[i] -= b[j] * A[i][j]
-            print(A)
-            print(b)
-
-    
-A = np.array([
-    [12., -51.,   4.],
-    [ 6., 167., -68.],
-    [-4.,  24., -41.]])
-
-b = np.array([10, 20, 30])
+    A1 = A.copy()
+    n = len(A1)
+    x = np.zeros(n)
+    x[n-1] = b[n-1] / A1[n-1][n-1]
+    for k in range(n-2, -1, -1):
+        sum = b[k]
+        for j in range(n-1, k, -1):
+            sum -= A1[k][j] * x[j]
+        x[k] = sum / A1[k][k]
+    print(x)
+    return x
+            
 
 A = np.array([
     [12., -51.,   4.],
     [ 6., 167., -68.],
-    [-4.,  24., -41.]])
+    [-4.,  24., -41]])
+b = np.array([10., 20., 30.])
 
-b = np.array([10, 20, 30])
-
-
-backsub(A, b)
-
-#print(np.linalg.qr(A))
-#print(np.linalg.solve(A, b))
-
-qr(A) 
-
- 
+print("npqr", np.linalg.qr(A))
+print("npsolve", np.linalg.solve(qr(A), b))
+#qr(A)
+backsub(qr(A), b)
