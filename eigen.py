8aefe085c600e943216e258ea9e89715163563a3
AlexaEgorova <37927173+AlexaEgorova@users.noreply.github.com>
2018-11-21 22:04:31 +0300

    Update eigen.py

--- eigen.py
+++ eigen.py
@@ -4,25 +4,15 @@ import math as m
 def norm(x):
     return m.sqrt(sum([x_i**2 for x_i in x]))
 
-
 def qr(A):
     n = len(A)
     Q = np.eye(n)
-    Ux = np.eye(n)
     for i in range(0, n-1):
         U = np.eye(n)
         U[i:, i:] = ufind(A[i:, i])
         Q = np.dot(Q, U)
-        Ux = np.dot(Ux, Q)
-        print(Ux is Q)
         A = np.dot(U, A)
-    #print("Q:\n", Q)
-    '''for i in range(0, n):
-        for j in range(0, n):
-            A[i][j] = round(A[i][j], 8)'''
-    #print("A:\n", A)
-    return Q, A, Ux
-
+    return Q, A
 
 def ufind(cln):
     v = cln / (cln[0] + np.copysign(norm(cln), cln[0]))
@@ -31,48 +21,33 @@ def ufind(cln):
     U -= (2 / np.dot(v, v)) * np.dot(v[:, None], v[None, :])
     return U
 
+def vectors(Z):
+    a = Z.copy()
+    x = (a + a.transpose()).copy()
+    pq = np.eye(Z.shape[0])
+    for i in range(30):
+        q, r = qr(x)
+        pq = np.dot(pq, q)
+        x = np.dot(r, q)
+    x = np.around(x, decimals=5)
+    return q, x, pq
 
-def check_diagonal(arr):
-    for i in range(len(arr)):
-        for j in range(len(arr[i])):
-            if i == j:
-                continue
-            else:
-                if abs(arr[i][j]) > 1e-3:
-                    return False
-    return True
-
-
-'''def qr_factorization(arr):
-    temp = arr
-    i = 0
-    while True:
-        q, r = qr(temp)
-        temp = np.dot(r, q)
-        if check_diagonal(temp):
-            break
-        i += 1
-    return temp'''
-
-A = np.array([
-    [2.,-1., 3.],
-    [1., 0., 5.],
-    [0., 1., 6]])
+F = np.array([
+    [52., 30., 49., 28.],
+    [30., 50., 8., 44.],
+    [49., 8., 46., 16.],
+    [28., 44., 16., 22.]])
 
 B = np.array([[-2., 1.], [1., -1.]])
 
-eigens = np.diag(np.dot(R, Q))
-veigens = np.eye(2, len(eigens))
-z = B.copy()
-for i in range(0, len(eigens)):
-    z[i][i] -= eigens[i]
-print("z:\n", z)    
+Q, x, Ux = vectors(F)
+print("X:\n", x/2)
+print("Ux:\n", Ux)   
 
-print("eigens\n", eigens)
-print("qr_lin\n", np.linalg.qr(B))
-print("eig_lin\n", np.linalg.eig(B), "\n")
-#print("fac", qr_factorization(B), "\n")
-Q, R, Ux = qr(B)
+print("qr_lin\n", np.linalg.qr(F))
+print("eig_lin\n", np.linalg.eig(F), "\n")
+
+Q, R = qr(F)
+
 print("Q:\n", Q)
 print("R:\n", R)
-print("Ux:\n", Ux)
